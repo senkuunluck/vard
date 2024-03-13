6e918166dc07fe08313ef65c851871b524909085
IBorunov <ivanborun@gmail.com>
2024-03-13 22:22:42 +0300

    Сделали БД

--- vard/models.py
+++ vard/models.py
@@ -2,51 +2,70 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+class Users(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    date_creation = models.DateTimeField(auto_now_add=True)
+    date_passwords_change = models.DateTimeField(auto_now=True)
+    password = models.CharField(max_length=255)
+
+
+class Place(models.Model):
+    type = models.CharField(max_length=255)
+
+class Access_type(models.Model):
+    access_type = models.CharField(max_length=255)
+
+class Files_type(models.Model):
+    files_type = models.CharField(max_length=255)
+
 class Files(models.Model):
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
-    place_id = models.AutoField()
-    type_id = models.AutoField()
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    type = models.ForeignKey(Access_type, on_delete=models.CASCADE)
     date_creation = models.DateTimeField(auto_now_add=True)
     date_change = models.DateTimeField(auto_now=True)
     date_delete = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, unique=True)
     link = models.CharField(max_length=255, unique=True)
-    publish = models.CharField(max_length=1, choices=TYPES)
+    publish = models.BooleanField(default=True) #TODO
 
 class Access(models.Model):
-    id_file = models.OneToOneField(Files, on_delete=models.CASCADE)
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
-    access_type_id = models.AutoField()
+    id_file = models.ForeignKey(Files, on_delete=models.CASCADE)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    access_type_id = models.ForeignKey(Access_type, on_delete=models.CASCADE)
     date_access_open = models.DateTimeField(auto_now=True)
     date_access_close = models.DateTimeField(auto_now=True)
 
 class Feedback (models.Model):
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
     date_creation = models.DateTimeField(auto_now_add=True)
     theme = models.CharField(max_length=255)
     description = models.TextField()
 
 class Dushboards(models.Model):
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
     date_creation = models.DateTimeField(auto_now_add=True)
     date_change = models.DateTimeField(auto_now=True)
 
 class Charts(models.Model):
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
     date_creation = models.DateTimeField(auto_now_add=True)
     date_change = models.DateTimeField(auto_now=True)
 
 class Comments(models.Model):
-    file_id = models.OneToOneField(Files, on_delete=models.CASCADE)
-    chart_id = models.OneToOneField(Charts, on_delete=models.CASCADE)
-    dushboard_id = models.OneToOneField(Dushboards, on_delete=models.CASCADE)
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    file = models.ForeignKey(Files, on_delete=models.CASCADE)
+    chart = models.ForeignKey(Charts, on_delete=models.CASCADE)
+    dushboard = models.ForeignKey(Dushboards, on_delete=models.CASCADE)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
     date_send = models.DateTimeField(auto_now=True)
     date_remove = models.DateTimeField(auto_now=True)
     date_delivery = models.DateTimeField(auto_now=True)
     comment = models.TextField()
 
-class ReadComments(models.Model):
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
-    comment_id = models.OneToOneField(Comments, on_delete=models.CASCADE)
+class ReadComments(models.Model): #TODO
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comments, on_delete=models.CASCADE)
     date_reading = models.DateTimeField(auto_now=True)
+
