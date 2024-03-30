17e9ab06060790a4912c64facbc184d0661f4143
IBorunov <ivanborun@gmail.com>
2024-03-30 20:01:10 +0300

    Делаем загрузку файлов

--- uploadfiles/serializers.py
+++ uploadfiles/serializers.py
@@ -7,7 +7,3 @@ class FileUploadSerializer(serializers.ModelSerializer):
     class Meta:
         model = File
         fields = '__all__'
-        # не обрабатываем поле file_type.
-        # Нельзя давать пользователю указывать произвольный тип файла, нужно вытаскивать его самим
-        #см. views
-#
